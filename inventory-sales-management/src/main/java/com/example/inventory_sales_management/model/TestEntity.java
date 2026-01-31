d6617052817fb2fb2b22d98a11043b970b6f7d5f
aniket bhoge <aniketbhoge04@gmail.com>
2026-01-31 15:39:47 +0530

    feat: add Product entity with basic fields

---
+++ inventory-sales-management/src/main/java/com/example/inventory_sales_management/model/TestEntity.java
@@ -0,0 +1,10 @@
+package com.example.inventory_sales_management.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+public class TestEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+}
