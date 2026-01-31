2f249df0bfcf81c7024ddcd3bb289b3a7e84e2bb
aniket bhoge <aniketbhoge04@gmail.com>
2026-01-31 11:37:20 +0530

    Initialize Inventory and Sales Management Spring Boot project

---
+++ inventory-sales-management/src/main/java/com/example/inventory_sales_management/InventorySalesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventory_sales_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySalesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorySalesManagementApplication.class, args);
+	}
+
+}
---
+++ inventory-sales-management/src/test/java/com/example/inventory_sales_management/InventorySalesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inventory_sales_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySalesManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
