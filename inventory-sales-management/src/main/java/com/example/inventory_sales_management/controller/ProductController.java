d305c7abb1112518bd4fcc741afd0c2dc6660177
aniket bhoge <aniketbhoge04@gmail.com>
2026-02-01 22:17:23 +0530

    refactor: use ResponseEntity in ProductController for CRUD APIs

--- inventory-sales-management/src/main/java/com/example/inventory_sales_management/controller/ProductController.java
+++ inventory-sales-management/src/main/java/com/example/inventory_sales_management/controller/ProductController.java
@@ -4,6 +4,8 @@ import com.example.inventory_sales_management.model.Product;
 import com.example.inventory_sales_management.service.ProductService;
 import com.example.inventory_sales_management.service.ProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,32 +21,49 @@ public class ProductController {
 
     // Create
     @PostMapping
-    public Product createProduct(@RequestBody Product product){
-        return productService.createProduct(product);
+    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+        Product savedProduct = productService.createProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
     }
 
     // Read all
     @GetMapping
-    public List<Product> getAllProducts(){
-        return  productService.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts(){
+        return ResponseEntity.ok(productService.getAllProducts());
     }
 
     // Read by Id
     @GetMapping("/{id}")
-    public Product getProductById(@PathVariable Long id){
-        return productService.getProductById(id);
+    public ResponseEntity<?> getProductById(@PathVariable Long id){
+        try{
+            Product product = productService.getProductById(id);
+            return ResponseEntity.ok(product);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body("Product not found with id " + id);
+        }
     }
 
     // Update
     @PutMapping("/{id}")
-    public Product updateProduct(@PathVariable Long id,@RequestBody Product product){
-        return productService.updateProduct(id, product);
+    public ResponseEntity<?> updateProduct(@PathVariable Long id,@RequestBody Product product){
+        try{
+            Product updated = productService.updateProduct(id , product);
+            return ResponseEntity.ok(updated);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found with id " + id);
+        }
     }
 
     // Delete
     @DeleteMapping("/{id}")
-    public void deleteProduct(@PathVariable Long id){
-         productService.deleteProduct(id);
+    public ResponseEntity<?> deleteProduct(@PathVariable Long id){
+        try{
+            productService.deleteProduct(id);
+            return ResponseEntity.noContent().build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found with id " + id);
+        }
     }
 
 }
