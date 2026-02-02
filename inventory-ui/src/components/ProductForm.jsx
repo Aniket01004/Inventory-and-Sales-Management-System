8da3939ad9a07e37a5c27533c87e0f609637bb3d
aniket bhoge <aniketbhoge04@gmail.com>
2026-02-02 22:24:52 +0530

    feat: add basic React Components and JSX structure

---
+++ inventory-ui/src/components/ProductForm.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function ProductForm() {
+  return (
+    <div>
+        <h2>Add Product</h2>
+        <input placeholder='Product Name' />
+        <br />
+        <input placeholder='Price' />
+        <br />
+        <button>Add</button>
+
+    </div>
+  );
+}
+
+export default ProductForm
