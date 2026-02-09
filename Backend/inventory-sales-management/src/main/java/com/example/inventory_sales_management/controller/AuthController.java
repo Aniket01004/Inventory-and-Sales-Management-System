8a9864659fa17de40a1b5ff0f92f917a688347e7
aniket bhoge <aniketbhoge04@gmail.com>
2026-02-09 16:23:36 +0530

    feat: secure backend APIs using JWT authentication

--- Backend/inventory-sales-management/src/main/java/com/example/inventory_sales_management/controller/AuthController.java
+++ Backend/inventory-sales-management/src/main/java/com/example/inventory_sales_management/controller/AuthController.java
@@ -1,27 +1,42 @@
 package com.example.inventory_sales_management.controller;
 
 import com.example.inventory_sales_management.model.User;
+import com.example.inventory_sales_management.security.JwtUtil;
 import com.example.inventory_sales_management.service.AuthService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
-    public AuthController(AuthService authService){
+    private final JwtUtil jwtUtil;
+
+    public AuthController(AuthService authService, JwtUtil jwtUtil) {
         this.authService = authService;
+        this.jwtUtil = jwtUtil;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login (@RequestBody User user){
-        boolean success = authService.login(user.getUsername(), user.getPassword());
-        if(success){
-            return ResponseEntity.ok("Login successful");
-        }else{
-            return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or Password");
+    public ResponseEntity<?> login(@RequestBody User user) {
+
+        boolean isValid =
+                authService.login(user.getUsername(), user.getPassword());
+
+        if (!isValid) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body("Invalid username or password");
         }
+
+        String token = jwtUtil.generateToken(user.getUsername());
+
+        return ResponseEntity.ok(
+                Map.of("token", token)
+        );
     }
 }
