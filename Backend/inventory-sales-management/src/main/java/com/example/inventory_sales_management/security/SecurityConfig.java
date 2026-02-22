7fa8f5685aad6e53c0f8750f219123ffe91c6c64
aniket bhoge <aniketbhoge04@gmail.com>
2026-02-22 10:30:19 +0530

    configure backend for cloud deployment

--- Backend/inventory-sales-management/src/main/java/com/example/inventory_sales_management/security/SecurityConfig.java
+++ Backend/inventory-sales-management/src/main/java/com/example/inventory_sales_management/security/SecurityConfig.java
@@ -50,7 +50,10 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://your-vercel-app.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
